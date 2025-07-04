9f410b9c0d135c4485862ac7d289bb704a6ba088
Luchardiles <luiis_rapero@hotmail.cl>
2025-07-04 16:26:15 -0400

    fix: deleted consts in catchgrp

--- src/utils/catchGrpc.js
+++ src/utils/catchGrpc.js
@@ -1,13 +1,11 @@
-const grpc = require("@grpc/grpc-js");
-const { toGrpcError } = require("./grpcUtils");
 function catchGrpc(fn) {
   return async (call, callback) => {
     try {
       await fn(call, callback);
     } catch (err) {
       const errObj = err.isOperational
-        ? toGrpcError(err)
-        : { code: grpc.status.INTERNAL, message: "Internal server error" };
+        ? { code: err.statusCode, details: err.message }
+        : { code: 500, details: "Internal server error" };
       callback(errObj, null);
     }
   };
